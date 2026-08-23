63ae2ba7e4c5c37bf8e3d1781a49014591a86103
beyonddprint35-pixel <beyonddprint35@gmail.com>
2026-08-23 19:40:13 +0300

    Add dedicated 3D printing route page

---
+++ frontend/src/pages/ThreeDPrinting.jsx
@@ -0,0 +1,12 @@
+import Home from "./Home";
+import "./ThreeDPrinting.css";
+
+function ThreeDPrinting() {
+  return (
+    <div className="three-d-printing-page">
+      <Home />
+    </div>
+  );
+}
+
+export default ThreeDPrinting;
