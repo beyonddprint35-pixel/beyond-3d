ebf8949689fa5d387a38091bd7765521b2ce98b1
beyonddprint35-pixel <beyonddprint35@gmail.com>
2026-08-23 23:52:21 +0300

    Remove duplicate homepage navigation header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,6 +26,7 @@ import "./pages/MenuHomeWordmark.css";
 import "./pages/MenuHomeDarkGlow.css";
 import "./pages/MenuHomeAccessRealism.css";
 import "./pages/MenuHomeAccessInteraction.css";
+import "./pages/MenuHomeNavCleanup.css";
 import "./pages/AdminTheme.js";
 import "./pages/AdminTheme.css";
 
