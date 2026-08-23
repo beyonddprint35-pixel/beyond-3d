308b066a4beadbbb93aee36294e8576f21313d8e
beyonddprint35-pixel <beyonddprint35@gmail.com>
2026-08-23 19:53:12 +0300

    Use refined menu homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import {
   Routes,
 } from "react-router-dom";
 
-import MenuHome from "./pages/MenuHome";
+import MenuHome from "./pages/MenuHomeRefined";
 import ThreeDPrinting from "./pages/ThreeDPrinting";
 import Admin from "./pages/Admin";
 import OrderDetails from "./pages/OrderDetails";
@@ -14,71 +14,20 @@ import MenuMobilePreviewPage from "./pages/MenuMobilePreviewPage";
 
 import "./pages/MenuHomeOverrides.css";
 
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        <Route
-          path="/"
-          element={
-            <MenuHome />
-          }
-        />
-
-
-        <Route
-          path="/3DPRINTING"
-          element={
-            <ThreeDPrinting />
-          }
-        />
-
-
-        <Route
-          path="/menu-builder"
-          element={
-            <MenuBuilder />
-          }
-        />
-
-
-        <Route
-          path="/menu-mobile-preview"
-          element={
-            <MenuMobilePreviewPage />
-          }
-        />
-
-
-        <Route
-          path="/admin"
-          element={
-            <Admin />
-          }
-        />
-
-
-        <Route
-          path="/admin/order/:id"
-          element={
-            <OrderDetails />
-          }
-        />
-
-
-        <Route
-          path="/track"
-          element={
-            <TrackOrder />
-          }
-        />
-
+        <Route path="/" element={<MenuHome />} />
+        <Route path="/3DPRINTING" element={<ThreeDPrinting />} />
+        <Route path="/menu-builder" element={<MenuBuilder />} />
+        <Route path="/menu-mobile-preview" element={<MenuMobilePreviewPage />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin/order/:id" element={<OrderDetails />} />
+        <Route path="/track" element={<TrackOrder />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
 
 export default App;
