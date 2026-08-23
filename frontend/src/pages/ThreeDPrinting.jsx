d10ee73d107d77f51b2c6d9e8042edefdbb6f420
beyonddprint35-pixel <beyonddprint35@gmail.com>
2026-08-23 19:33:19 +0300

    Separate 3D printing experience from menu homepage

--- frontend/src/pages/ThreeDPrinting.jsx
+++ frontend/src/pages/ThreeDPrinting.jsx
@@ -1,3 +1,10 @@
 import Home from "./Home";
+import "./ThreeDPrinting.css";
 
-export default Home;
+export default function ThreeDPrinting() {
+  return (
+    <div className="three-d-printing-page">
+      <Home />
+    </div>
+  );
+}
