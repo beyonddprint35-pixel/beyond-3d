d737db024f4a13ae60e66644938cc569dfdd1cc4
beyonddprint35-pixel <beyonddprint35@gmail.com>
2026-08-29 23:46:33 +0300

    Replace BuddyMasterChef with Tacos Buddy image

--- frontend/src/components/BuddyMasterChef.jsx
+++ frontend/src/components/BuddyMasterChef.jsx
@@ -1,39 +1,7 @@
-import {
-  Suspense,
-  useMemo,
-} from "react";
-
-import {
-  Canvas,
-} from "@react-three/fiber";
-
-import {
-  Center,
-  useGLTF,
-} from "@react-three/drei";
-
 import "./BuddyMasterChef.css";
 
-const BUDDY_MASTER_CHEF_MODEL = "/models/buddymasterchef-blue-web.glb";
-
-function BuddyMasterChefModel() {
-  const { scene } =
-    useGLTF(BUDDY_MASTER_CHEF_MODEL);
-
-  const model = useMemo(
-    () => scene.clone(true),
-    [scene]
-  );
-
-  return (
-    <Center>
-      <primitive
-        object={model}
-        scale={0.82}
-      />
-    </Center>
-  );
-}
+const TACOS_BUDDY_IMAGE =
+  "/images/tacos-buddy-phone.webp";
 
 export default function BuddyMasterChef() {
   return (
@@ -41,38 +9,21 @@ export default function BuddyMasterChef() {
       className="buddy-master-chef"
       aria-hidden="true"
     >
-      <Canvas
-        camera={{
-          position: [0, 0.05, 5.6],
-          fov: 30,
+      <img
+        src={TACOS_BUDDY_IMAGE}
+        alt=""
+        draggable="false"
+        decoding="async"
+        style={{
+          display: "block",
+          width: "100%",
+          height: "100%",
+          objectFit: "contain",
+          objectPosition: "center bottom",
+          userSelect: "none",
+          pointerEvents: "none",
         }}
-        gl={{
-          alpha: true,
-          antialias: true,
-        }}
-        dpr={[1, 2]}
-      >
-        <ambientLight intensity={2.4} />
-
-        <directionalLight
-          position={[3, 5, 5]}
-          intensity={2.6}
-        />
-
-        <directionalLight
-          position={[-3, 2, 4]}
-          intensity={0.9}
-        />
-
-        <Suspense fallback={null}>
-          <BuddyMasterChefModel />
-        </Suspense>
-      </Canvas>
+      />
     </div>
   );
 }
-
-/*
- * Intentionally NO useGLTF.preload().
- * Master Chef is near the bottom of the page.
- */
