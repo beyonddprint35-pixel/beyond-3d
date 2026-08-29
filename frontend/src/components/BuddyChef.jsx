7f83be3ed9d380324a976fa9307893a2fcbeb8e0
beyonddprint35-pixel <beyonddprint35@gmail.com>
2026-08-29 23:07:09 +0300

    Replace waiter Buddy renderer with Pizza Buddy image

--- frontend/src/components/BuddyChef.jsx
+++ frontend/src/components/BuddyChef.jsx
@@ -1,70 +1,7 @@
-import {
-  Suspense,
-  useEffect,
-  useMemo,
-} from "react";
-
-import {
-  Canvas,
-  useThree,
-} from "@react-three/fiber";
-
-import {
-  Center,
-  useGLTF,
-} from "@react-three/drei";
-
-import * as THREE from "three";
-
 import "./BuddyChef.css";
 
-const BUDDY_CHEF_MODEL = "/models/buddyChef-blue-web.glb";
-
-function BuddyChefModel() {
-  const {
-    scene,
-  } = useGLTF(BUDDY_CHEF_MODEL);
-
-  const model = useMemo(
-    () => scene.clone(true),
-    [scene]
-  );
-
-  useEffect(() => {
-    model.traverse((object) => {
-      if (!object.isMesh) return;
-
-      object.frustumCulled = true;
-
-      if (object.material) {
-        object.material.side = THREE.FrontSide;
-      }
-    });
-  }, [model]);
-
-  return (
-    <group position={[0.08, 0, 0]}>
-      <Center>
-        <primitive
-          object={model}
-          scale={1.00}
-        />
-      </Center>
-    </group>
-  );
-}
-
-function TransparentScene() {
-  const {
-    gl,
-  } = useThree();
-
-  useEffect(() => {
-    gl.setClearColor(0x000000, 0);
-  }, [gl]);
-
-  return null;
-}
+const PIZZA_BUDDY_IMAGE =
+  "/images/pizza-buddy-laptop.webp";
 
 export default function BuddyChef() {
   return (
@@ -72,37 +9,21 @@ export default function BuddyChef() {
       className="buddy-chef"
       aria-hidden="true"
     >
-      <Canvas
-        camera={{
-          position: [0, 0.05, 4.8],
-          fov: 30,
+      <img
+        src={PIZZA_BUDDY_IMAGE}
+        alt=""
+        draggable="false"
+        decoding="async"
+        style={{
+          display: "block",
+          width: "100%",
+          height: "100%",
+          objectFit: "contain",
+          objectPosition: "center bottom",
+          userSelect: "none",
+          pointerEvents: "none",
         }}
-        gl={{
-          alpha: true,
-          antialias: true,
-          powerPreference: "high-performance",
-        }}
-        dpr={[1.5, 2]}
-      >
-        <TransparentScene />
-
-        <ambientLight intensity={2.4} />
-
-        <directionalLight
-          position={[3, 5, 5]}
-          intensity={2.6}
-        />
-
-        <directionalLight
-          position={[-3, 2, 4]}
-          intensity={0.9}
-        />
-
-        <Suspense fallback={null}>
-          <BuddyChefModel />
-        </Suspense>
-      </Canvas>
+      />
     </div>
   );
 }
-
