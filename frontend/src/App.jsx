5e94cb7e8edbcb89cd5445d4b699825f6783d891
beyonddprint35-pixel <beyonddprint35@gmail.com>
2026-09-04 23:48:51 +0300

    Defer business pricing choice until publish

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -44,6 +44,7 @@ import "./components/MyAccountLightMode.css";
 import "./components/MyAccountMenuDraftDelete.js";
 import "./components/MyAccountMenuDraftDelete.css";
 import "./components/AuthModalSimplified.css";
+import "./components/AuthPricingDeferred.css";
 import "./pages/MenuHomeAccessInteraction.js";
 import "./pages/MenuHomeOverrides.css";
 import "./pages/MenuHomeFinalPatch.css";
