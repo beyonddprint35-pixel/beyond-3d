3fe8fb5eefcbe5389d5f4e1948d1e94311922046
beyonddprint35-pixel <beyonddprint35@gmail.com>
2026-09-06 23:12:33 +0300

    Apply full-screen menu regulation globally

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -20,6 +20,7 @@ import "./components/BeyondLiveMenuDesign.css";
 import "./components/BeyondLiveMenuDesignMobilePatch.css";
 import "./pages/MenuStudioV3Controls.css";
 import "./features/menu-engine/renderer/menuTypographyGuard.css";
+import "./features/menu-engine/renderer/menuViewportFill.css";
 import "./styles/uiTypographyGuard.css";
 
 // Development must never be controlled by a previously-installed service worker.
