7d56ef233b346b2478c40bf7f3fe947a325e1605
beyonddprint35-pixel <beyonddprint35@gmail.com>
2026-09-07 00:27:35 +0300

    Install Content visibility scroll guard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,6 +16,7 @@ import installUiTypographyGuard from "./styles/uiTypographyGuard";
 import installMenuContentAdvancedAlwaysOpen from "./features/menu-engine/studio/menuContentAdvancedAlwaysOpen";
 import installMenuContentCategoryAccordion from "./pages/menuContentCategoryAccordion";
 import installMenuContentPreviewSelection from "./pages/menuContentPreviewSelection";
+import installMenuContentVisibilityScrollGuard from "./pages/menuContentVisibilityScrollGuard";
 import installMenuStudioViewportLock from "./pages/menuStudioViewportLock";
 import "./components/BeyondMenuPlatform.css";
 import "./components/BeyondLiveMenuDesign.css";
@@ -45,6 +46,7 @@ installUiTypographyGuard();
 installMenuContentAdvancedAlwaysOpen();
 installMenuContentCategoryAccordion();
 installMenuContentPreviewSelection();
+installMenuContentVisibilityScrollGuard();
 installMenuStudioViewportLock();
 
 createRoot(document.getElementById('root')).render(
